2cf1fde5deabef123512d8ca49ababb2634e806e
Tiago Hellebrandt Silva <thellebrandtsilva@gmail.com>
2018-09-09 23:43:09 -0400

    create initial templates.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# requests_system
-Sistema de pedidos.
+# Requests system
+
+## Descrição.
+Este projeto PHP é um sistema de pedidos básico que inclui um sistema de controle de clientes, produtos e pedidos.
---
+++ base-head.php
@@ -0,0 +1,15 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<!-- Materialize CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+
+<!-- Materialize JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+
+<!-- Material Icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- Ionic Icons -->
+<link href="https://unpkg.com/ionicons@4.4.2/dist/css/ionicons.min.css" rel="stylesheet">
